45d59402a02baea16709e2f8d49ff2ccab0692da
Edson Santana Alves <edson_santana08@outlook.com>
2025-04-23 21:15:28 -0300

    Adicionando CORS ao backend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // Importa o pacote cors
 const connectDB = require('./config/db');
 const app = express();
 const authRoutes = require('./routes/authRoutes');
@@ -9,6 +10,12 @@ const evidenceRoutes = require('./routes/evidenceRoutes'); // Adiciona a importa
 const laudoRoutes = require('./routes/laudoRoutes');
 const relatorioRoutes = require('./routes/relatorioRoutes');
 
+// Configuração do CORS para permitir acesso do frontend no Netlify
+app.use(cors({
+  origin: 'https://https://laudo-pericias.netlify.app/', // URL do seu frontend no Netlify
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 connectDB();
 app.use(express.json({ extended: false }));
@@ -19,7 +26,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/evidences', evidenceRoutes); // Adiciona a utilização das rotas de evidências
 app.use('/api/laudos', laudoRoutes);
 app.use('/api/relatorios', relatorioRoutes);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
